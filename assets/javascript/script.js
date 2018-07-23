af7bc11b7009692890c9a5940d2ef1804b74f555
KatHe <he.katherine321@gmail.com>
2018-07-23 19:31:17 -0400

    so far

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    var config = {
+        apiKey: "AIzaSyBbQiEupEs4N2UfQZaGKa-HglWFN59UqBw",
+        authDomain: "employee-ccaad.firebaseapp.com",
+        databaseURL: "https://employee-ccaad.firebaseio.com",
+        projectId: "employee-ccaad",
+        storageBucket: "employee-ccaad.appspot.com",
+        messagingSenderId: "859962902160"
+      };
+      firebase.initializeApp(config);
+})
