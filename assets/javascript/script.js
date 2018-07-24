9eb1ee33b4d7174ca503c29fbfd2a6d33dc24c6b
KatHe <he.katherine321@gmail.com>
2018-07-23 20:16:10 -0400

    working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,36 @@
-$(function() {
-    var config = {
-        apiKey: "AIzaSyBbQiEupEs4N2UfQZaGKa-HglWFN59UqBw",
-        authDomain: "employee-ccaad.firebaseapp.com",
-        databaseURL: "https://employee-ccaad.firebaseio.com",
-        projectId: "employee-ccaad",
-        storageBucket: "employee-ccaad.appspot.com",
-        messagingSenderId: "859962902160"
-      };
-      firebase.initializeApp(config);
-})
+
+var config = {
+    apiKey: "AIzaSyCcnYsUl9ZJ-xeYx9m4Pyl9F_9yTW6oDqI",
+    authDomain: "my-awson-project.firebaseapp.com",
+    databaseURL: "https://my-awson-project.firebaseio.com",
+    projectId: "my-awson-project",
+    storageBucket: "my-awson-project.appspot.com",
+    messagingSenderId: "1045330612371"
+    };
+    firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    console.log("hello");
+    
+
+    // Grabbed values from text boxes
+    name = $("#name").val().trim();
+    role = $("#role").val().trim();
+    date = $("#start-date").val().trim();
+    salary = $("#salary").val().trim();
+
+
+    // Code for handling the push
+    database.ref().set({
+      name: name,
+      role: role,
+      date: date,
+      salary: salary,
+    });
+  });
 
 
 $( document ).ready(function() {
