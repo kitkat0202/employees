ff6372435623798e58f7fbccfd1112ae065830cd
KatHe <he.katherine321@gmail.com>
2018-07-23 20:47:26 -0400

    stuff

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-
+var count = 0
 var config = {
     apiKey: "AIzaSyCcnYsUl9ZJ-xeYx9m4Pyl9F_9yTW6oDqI",
     authDomain: "my-awson-project.firebaseapp.com",
@@ -13,8 +13,6 @@ var database = firebase.database();
 
 $("#submit").on("click", function(event) {
     event.preventDefault();
-    console.log("hello");
-    
 
     // Grabbed values from text boxes
     name = $("#name").val().trim();
@@ -22,15 +20,31 @@ $("#submit").on("click", function(event) {
     date = $("#start-date").val().trim();
     salary = $("#salary").val().trim();
 
-
     // Code for handling the push
-    database.ref().set({
+    database.ref().push({
       name: name,
       role: role,
       date: date,
       salary: salary,
     });
-  });
+});
+
+database.ref().on("child_added", function(snapshot) {
+    
+    var sv = snapshot.val();
+    console.log(sv.name);
+    
+    
+    var monthWorked = 0
+    var total = 0
+
+    $(".table tbody").append($("<tr>").addClass(count).html(`<td>${sv.name}</td><td>${sv.role}</td><td>${sv.date}</td><td>${monthWorked}</td><td>${salary/12}</td><td>${total}</td>`))
+    // Change the HTML to reflect
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
 
 
 $( document ).ready(function() {
